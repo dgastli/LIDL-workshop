b036c7923bf9d589953e2a30fa5ea515b642084b
dgastli <gastli.dorra@mail.com>
2015-06-24 17:38:06 +0200

    exemple17 fonctionnel

--- ProjetEssai/src/exemple17.jsx
+++ ProjetEssai/src/exemple17.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var esprima = require('esprima');
+var Griddle = require('griddle-react');
 
 var estraverse = require('estraverse');
 
@@ -14,57 +15,128 @@ var App = React.createClass({
   onClick:function(){
 
     var code=React.findDOMNode(this.refs.textareaPas).value;
-  //  var syntaxe =JSON.stringify(esprima.parse(code), null, 4);
-    var syntaxe =JSON.stringify(esprima.parse('var answer = 42'), null, 4);
-
+    var syntaxe =esprima.parse(code);
+    //var syntaxe =esprima.parse('var answer = 42; x = 5; y = 1 + 3; z = 7 - 1; var nom = "dorra"; y = x; t=x+y;x=10*3;x=x+1;x=1+x;personne=nom+"gastli";');
+    console.log(JSON.stringify(esprima.parse('var answer = 42; x = 5; y = 1 + 3;z=7-1;var nom = "dorra"; y = x;x=10*3;x=x+1;x=1+x;'), null, 4));
     var ancien =this.state.data;
     estraverse.traverse(syntaxe, {
+
       enter: function(node){
+
         switch(node.type){
+
+
 
           case 'VariableDeclarator':
             ancien.push({
-              "Variable":node.declarations.id.name,
-              "Valeur":node.declarations.init.value,
+              "Variable":node.id.name,
+              "Valeur":node.init.value,
             });
             break;
 
+
+
+
          case 'ExpressionStatement' :
-          var operator1=node.expression.operator;
-          var operator2=node.expression.right.operator;
-          var operand1=node.expression.right.left.value;
-          var operand2=node.expression.right.right.value;
-          var valeur=0;
-          switch(operator2){
-            case '+':
-              valeur=parseInt(operand1)+parseInt(operand2);
-              break;
-            case '-':
-              valeur=parseInt(operand1)-parseInt(operand2);
-              break;
-            case '/':
-              valeur=parseInt(operand1)/parseInt(operand2);
-              break;
-            case '*':
-              valeur=parseInt(operand1)*parseInt(operand2);
-              break;
-            case '%':
-              valeur=parseInt(operand1) % parseInt(operand2);
-              break;
 
+
+         if(node.expression.right.type=='Literal'){
+          var operator=node.expression.operator;
+          ancien.push({
+
+            "Variable":node.expression.left.name,
+            "Valeur":node.expression.right.value,
+          });
+
+
+
+       }else if(node.expression.right.type=='BinaryExpression'){
+
+
+          if(node.expression.right.left.type=='Literal'){
+            var operand1=node.expression.right.left.value;
+          }else if (node.expression.right.left.type=='Identifier'){
+            var compteur =ancien.length-1;
+            while(ancien[compteur].Variable!=node.expression.right.left.name){
+              compteur--;
+            }
+            if(isNaN(parseInt(ancien[compteur].Valeur))){
+              var operand1=ancien[compteur].Valeur;
+            }else{
+              var operand1=parseInt(ancien[compteur].Valeur);
+            }
           }
-         ancien.push({
 
-           "Variable":node.expression.left.name,
-           "Valeur":valeur,
-         });
+
+
+
+          if(node.expression.right.right.type=='Literal'){
+            var operand2=node.expression.right.right.value;
+          }else if (node.expression.right.right.type=='Identifier'){
+            var compteur =ancien.length-1;;
+            while(ancien[compteur].Variable!=node.expression.right.right.name){
+              compteur--;
+            }
+            if(isNaN(parseInt(ancien[compteur].Valeur))){
+              var operand2=ancien[compteur].Valeur;
+            }else{
+              var operand2=parseInt(ancien[compteur].Valeur);
+            }
+          }
+
+
+         var operatorBinary=node.expression.right.operator;
+         var valeur=0;
+         switch(operatorBinary){
+           case '+':
+             valeur=operand1+operand2;
+             console.log(valeur);
+             break;
+           case '-':
+             valeur=operand1-operand2;
+             break;
+           case '/':
+             valeur=operand1/operand2;
+             break;
+           case '*':
+             valeur=operand1*operand2;
+             break;
+           case '%':
+             valeur=operand1 % operand2;
+             break;
+          }
+          ancien.push({
+
+            "Variable":node.expression.left.name,
+            "Valeur":valeur,
+          });
+
+
+
+
+         }else if(node.expression.right.type=='Identifier'){
+           var compteur =ancien.length-1;;
+           while(ancien[compteur].Variable!=node.expression.right.name){
+             compteur--;
+           }
+           ancien.push({
+
+             "Variable":node.expression.left.name,
+             "Valeur":ancien[compteur].Valeur,
+           });
+
+
+       }
          break;
-      }
 
+    }
     }
     });
     this.setState({data:ancien});
   },
+
+
+
 
 
   render: function() {
@@ -76,7 +148,7 @@ var App = React.createClass({
           ref="textareaPas"
           defaultValue={"Entrez votre texte "} />
         <button type="button" onClick={this.onClick} > OK </button>
-        <App results={this.state.data}/>
+        <Griddle results={this.state.data}/>
       </div>
     );
   }
